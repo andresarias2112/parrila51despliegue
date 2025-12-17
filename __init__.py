44871ccc6b948fef832852231e79f850b75a0426
Andresito Arias <andresitoarias96@gmail.com>
2025-12-17 14:17:27 -0500

    Mover serializer fuera de create_app

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 import pymysql
-pymysql.install_as_MySQLdb()  # Esto hace que Flask-MySQLdb use PyMySQL
+pymysql.install_as_MySQLdb()
 
 from flask import Flask
 from flask_mysqldb import MySQL
@@ -9,15 +9,14 @@ from itsdangerous import URLSafeTimedSerializer
 
 mysql = MySQL()
 mail = Mail()
+# Mover serializer FUERA de create_app
+serializer = URLSafeTimedSerializer(os.environ.get('SECRET_KEY', 'pinchellave'))
 
 def create_app():
     app = Flask(__name__, template_folder="template")
     
     # Secret key desde variables de entorno
     app.secret_key = os.environ.get('SECRET_KEY', 'pinchellave')
-    
-    # Serializer con la misma secret key
-    serializer = URLSafeTimedSerializer(app.secret_key)
 
     # ------------------ Configuración Base de Datos ------------------
     # Detectar si estamos en Railway (producción) o local (desarrollo)
