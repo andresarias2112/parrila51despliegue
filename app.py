b7abd777ea1bce8b1f80ab2872140791cd3aeb85
Andresito Arias <andresitoarias96@gmail.com>
2025-12-17 15:31:36 -0500

    Fix: Permitir que Gunicorn maneje producción

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ from __init__ import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    # Solo para desarrollo local, Railway usará Gunicorn
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
