99e926954169bf5155b94be7918e39a672c7355b
Andresito Arias <andresitoarias96@gmail.com>
2025-12-17 15:35:16 -0500

    Remover app.run para producción

--- app.py
+++ app.py
@@ -3,7 +3,5 @@ from __init__ import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    # Solo para desarrollo local, Railway usará Gunicorn
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+# NO ejecutar app.run() en producción
+# Gunicorn se encargará de esto
